5979344dbd88689848f06400634a27315c860a15
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2022-07-10 10:26:49 -0500

    Renamed Mover script to Movement

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class Mover : MonoBehaviour
+public class Movement : MonoBehaviour
 {
     [SerializeField] Transform target;
 
