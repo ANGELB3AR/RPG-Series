aba3afc2e983f18db18491af9172e25f5dcdef7f
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2022-07-11 10:33:53 -0500

    Configured gizmo to draw lines between waypoints

--- Assets/Scripts/Control/PatrolPath.cs
+++ Assets/Scripts/Control/PatrolPath.cs
@@ -13,9 +13,29 @@ namespace RPG.Control
         {
             for (int i = 0; i < transform.childCount; i++)
             {
+                int j = GetNextIndex(i);
                 Gizmos.color = Color.gray;
-                Gizmos.DrawSphere(transform.GetChild(i).transform.position, waypointGizmosRadius);
+                Gizmos.DrawSphere(GetWaypoint(i), waypointGizmosRadius);
+                Gizmos.DrawLine(GetWaypoint(i), GetWaypoint(j));
             }
+        }
+
+        int GetNextIndex(int i)
+        {
+            if (i < transform.childCount)
+            {
+                return i + 1;
+            }
+            else
+            {
+                return 0;
+            }
+            
+        }
+
+        Vector3 GetWaypoint(int i)
+        {
+            return transform.GetChild(i).transform.position;
         }
     }
 }
