e1e6b7f96ce10b8f0faea63fc6db885341f7644b
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2022-07-11 07:47:17 -0500

    Created chase distance check

--- Assets/Scripts/Control/AIController.cs
+++ Assets/Scripts/Control/AIController.cs
@@ -6,6 +6,16 @@ namespace RPG.Control
 {
     public class AIController : MonoBehaviour
     {
+        [SerializeField] float chaseDistance = 5f;
 
+        void Update()
+        {
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+            float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
+            if (distanceToPlayer <= chaseDistance)
+            {
+                Debug.Log(name + " should chase");
+            }
+        }
     }
 }
