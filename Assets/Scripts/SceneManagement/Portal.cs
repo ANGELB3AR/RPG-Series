585195b05a86976ff31ffc11536c61ac29a19782
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2022-07-13 13:33:58 -0500

    Added portals to both scenes

--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace RPG.SceneManagement
 {
     public class Portal : MonoBehaviour
     {
+        [SerializeField] int sceneToLoad = -1;
+
         void OnTriggerEnter(Collider other)
         {
             if (other.CompareTag("Player"))
             {
-                print("Portal triggered");
+                SceneManager.LoadScene(sceneToLoad);
             }
         }
     }
