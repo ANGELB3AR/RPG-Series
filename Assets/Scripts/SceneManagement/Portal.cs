40146886aad8dd005372ece56162b30b79f6d862
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2022-07-13 13:00:37 -0500

    Rebaked navmesh & scripted portal

--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -2,17 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Portal : MonoBehaviour
+namespace RPG.SceneManagement
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Portal : MonoBehaviour
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                print("Portal triggered");
+            }
+        }
     }
 }
