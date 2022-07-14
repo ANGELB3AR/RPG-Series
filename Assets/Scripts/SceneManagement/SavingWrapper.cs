8c9d22270ab5a49c0cb179b69c56d4a8fd0f4ff6
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2022-07-14 17:32:33 -0500

    Added saving GO & created saving wrapper file

---
+++ Assets/Scripts/SceneManagement/SavingWrapper.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.SceneManagement
+{
+    public class SavingWrapper : MonoBehaviour
+    {
+        void Update()
+        {
+
+        }
+    }
+}
