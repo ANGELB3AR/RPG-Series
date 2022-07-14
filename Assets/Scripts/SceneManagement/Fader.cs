055253502de5d93b4f94a532713cc6a614b2aa83
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2022-07-14 12:01:33 -0500

    Created fade out method

--- Assets/Scripts/SceneManagement/Fader.cs
+++ Assets/Scripts/SceneManagement/Fader.cs
@@ -6,6 +6,21 @@ namespace RPG.SceneManagement
 {
     public class Fader : MonoBehaviour
     {
+        CanvasGroup canvasGroup;
 
+        void Start()
+        {
+            canvasGroup = GetComponent<CanvasGroup>();
+            StartCoroutine(FadeOut(3f));
+        }
+
+        IEnumerator FadeOut(float time)
+        {
+            while (canvasGroup.alpha < 1)
+            {
+                canvasGroup.alpha += Time.deltaTime / time;
+                yield return null;
+            }
+        }
     }
 }
