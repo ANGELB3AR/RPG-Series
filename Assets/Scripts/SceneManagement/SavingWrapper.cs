2b80da9bc570b93406866bedf9b88d27a2b2e825
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2022-07-14 17:38:24 -0500

    Wrote save & load methods

--- Assets/Scripts/SceneManagement/SavingWrapper.cs
+++ Assets/Scripts/SceneManagement/SavingWrapper.cs
@@ -1,14 +1,42 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using RPG.Saving;
 
 namespace RPG.SceneManagement
 {
     public class SavingWrapper : MonoBehaviour
     {
+        SavingSystem savingSystem;
+        const string defaultSaveFile = "save";
+
+        void Awake()
+        {
+            savingSystem = GetComponent<SavingSystem>();
+        }
+
         void Update()
         {
+            if (Input.GetKeyDown(KeyCode.L))
+            {
+                Load();
+            }
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                Save();
+            }
 
+        }
+
+        void Load()
+        {
+            savingSystem.Load(defaultSaveFile);
+        }
+
+        void Save()
+        {
+            savingSystem.Save(defaultSaveFile);
         }
     }
 }
