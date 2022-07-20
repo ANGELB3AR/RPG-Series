ed08b300a5d44aa6b17c9cb13827e71795c32ecb
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2022-07-20 06:06:48 -0500

    Created option for homing projectiles

--- Assets/Scripts/Combat/Projectile.cs
+++ Assets/Scripts/Combat/Projectile.cs
@@ -9,15 +9,23 @@ namespace RPG.Combat
     public class Projectile : MonoBehaviour
     {
         [SerializeField] float projectileSpeed = 1f;
+        [SerializeField] bool isHoming = false;
         
         Health target = null;
         float damage = 0f;
+
+        void Start()
+        {
+            if (target == null) { return; }
+            AimAtTarget();
+        }
 
         void Update()
         {
             if (target == null) { return; }
 
-            AimAtTarget();
+            if (isHoming) { AimAtTarget(); }
+
             FireProjectile();
         }
 
