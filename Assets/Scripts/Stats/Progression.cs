9182c7dc8ca3ae3aa7e41808b2ceac394a415806
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2022-07-21 06:18:37 -0500

    Started formatting SO

--- Assets/Scripts/Stats/Progression.cs
+++ Assets/Scripts/Stats/Progression.cs
@@ -1,7 +1,24 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Progression", menuName = "Stats/New Progression", order = 0)]
-public class Progression : ScriptableObject
+namespace RPG.Stats
 {
+    [CreateAssetMenu(fileName = "Progression", menuName = "Stats/New Progression", order = 0)]
+    public class Progression : ScriptableObject
+    {
+        [SerializeField] ProgressionCharacterClass[] characterClasses = null;
 
+        [System.Serializable]
+        class ProgressionCharacterClass
+        {
+            [SerializeField] CharacterClass characterClass;
+            [SerializeField] Level[] levels;
+        }
+
+        [System.Serializable]
+        class Level
+        {
+            [SerializeField] float health;
+            [SerializeField] float attack;
+        }
+    }
 }
