b4fb096cb5624d8a90ce560e724b83c253ad3d17
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2022-09-18 12:04:52 -0500

    Implemented random item dropping & started drop library

---
+++ Assets/Scripts/Inventories/DropLibrary.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Inventories
+{
+    [CreateAssetMenu(menuName = ("RPG/Inventory/Drop Library"))]
+    public class DropLibrary : ScriptableObject
+    {
+        // - Drop Chance
+        // - Min drops
+        // - Max drops
+        // - Potential Drops
+        //   - Relative chance
+        //   - Min items
+        //   - Max items
+    }
+}
