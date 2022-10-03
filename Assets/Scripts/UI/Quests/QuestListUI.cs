4549178d8b31b9b20cb37fb2d2ee76dbfd248624
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2022-10-03 13:46:44 -0500

    Created logic to populate quest list

---
+++ Assets/Scripts/UI/Quests/QuestListUI.cs
@@ -0,0 +1,24 @@
+using RPG.Quests;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.UI.Quests
+{
+    public class QuestListUI : MonoBehaviour
+    {
+        [SerializeField] Quest[] tempQuests;
+        [SerializeField] QuestItemUI questPrefab;
+
+        void Start()
+        {
+            transform.DetachChildren();
+
+            foreach (Quest quest in tempQuests)
+            {
+                QuestItemUI uiInstance = Instantiate<QuestItemUI>(questPrefab, transform);
+                uiInstance.Setup(quest);
+            }
+        }
+    }
+}
--- Assets/Scripts/UI/Quests/QuestTooltipSpawner.cs
+++ Assets/Scripts/UI/Quests/QuestTooltipSpawner.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace RPG.UI
+namespace RPG.UI.Quests
 {
     public class QuestTooltipSpawner : TooltipSpawner
     {
