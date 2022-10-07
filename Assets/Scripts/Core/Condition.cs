e0e63e7a498928832654c046429389bd770f9c92
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2022-10-07 14:46:24 -0500

    Finished course

--- Assets/Scripts/Core/Condition.cs
+++ Assets/Scripts/Core/Condition.cs
@@ -7,19 +7,53 @@ namespace RPG.Core
     [System.Serializable]
     public class Condition
     {
-        [SerializeField] string predicate;
-        [SerializeField] string[] parameters;
+        [NonReorderable]
+        [SerializeField] Disjunction[] and;
 
         public bool Check(IEnumerable<IPredicateEvaluator> evaluators)
         {
-            foreach (var evaluator in evaluators)
+            foreach (Disjunction disjunction in and)
             {
-                bool? result = evaluator.Evaluate(predicate, parameters);
-                
-                if (result == null) { continue; }
-                if (result == false) { return false; }
+                if (!disjunction.Check(evaluators)) { return false; }
             }
             return true;
+        }
+
+        [System.Serializable]
+        class Disjunction
+        {
+            [NonReorderable]
+            [SerializeField] Predicate[] or;
+
+            public bool Check(IEnumerable<IPredicateEvaluator> evaluators)
+            {
+                foreach (Predicate predicate in or)
+                {
+                    if (predicate.Check(evaluators)) { return true; }
+                }
+                return false;
+            }
+        }
+
+        [System.Serializable]
+        class Predicate
+        {
+            [SerializeField] string predicate;
+            [NonReorderable]
+            [SerializeField] string[] parameters;
+            [SerializeField] bool negate = false;
+
+            public bool Check(IEnumerable<IPredicateEvaluator> evaluators)
+            {
+                foreach (var evaluator in evaluators)
+                {
+                    bool? result = evaluator.Evaluate(predicate, parameters);
+
+                    if (result == null) { continue; }
+                    if (result == negate) { return false; }
+                }
+                return true;
+            }
         }
     }
 }
